5063301ddac721b6c4e2791301d31c373c56dc6d
nguyenduchoan <nguyenduchoan.wru@gmail.com>
2023-06-20 18:48:39 +0700

    init project

---
+++ src/main/java/com/learngit/demo/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.learngit.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/learngit/demo/TestAutoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutoDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
